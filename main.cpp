41bcde4f25b3229a8a88ba7779cc8046f16e1da2
prestonmasseyblake <pmassey-blake@mail.csuchico.edu>
2019-10-14 16:42:34 -0700

    grocerystorevectorlist

--- main.cpp
+++ main.cpp
@@ -1,36 +1,40 @@
 //Author: Preston Massey-Blake
 #include<iostream>
 #include<string>
+#include <vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
-  string item;
-do 
-  {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+        vector<string> list; //array of 5 strings
+        char input;
+        string item;
+        int counter=0;
+        do 
+        {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+                if (input == 'a' || input =='A') {
+                        cout<<"What is the item?\n";
+                        cin>>item;
+                        list.push_back (item);
+                              
+                      }
+                
+        }
+        while(input != 'q' &&  input != 'Q');
+        if(list.size() == 0){
+cout<<"No items to buy!"<<endl; }
 
-  if (input == 'a' || input =='A') {
-  cout<<"What is the item? ";
-  cin>>item;}
-
-  if ( numItems  <= 4) {
-   list[numItems] = item;
-
-  numItems++;
-  }
-  if (numItems > 4){
-   cout<<"You'll need a bigger list!";
-  }
-  }
-while(input != 'q' &&  input != 'Q');
-  return 0;
+        else {
+        cout<<"==ITEMS TO BUY=="<<endl;
+        for(int i=0; i < list.size(); i++) {
+        cout<<counter+1<<" "<<list.at(i)<<endl;
+        counter++; 
+        }
+        }  return 0;
 }
